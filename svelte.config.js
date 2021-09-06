ccfe60f540c5fe603704273a46142233309508f8
SDBagel <sdbagel@gmail.com>
2021-09-05 19:58:58 -0700

    feat: Add adapter static for build

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,6 +8,11 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		target: 'body'
 	}
 };
